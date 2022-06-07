8484f3b40ed03f4b17b5c78d6f5bc8047f54206e
szymonekl <szymon4klos@gmail.com>
2022-06-07 10:49:57 +0200

    add Models to project Department and Emploee

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace my_new_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _confiugration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _confiugration = configuration;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
 
 namespace my_new_app
 {
@@ -20,6 +21,17 @@ namespace my_new_app
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //ENable Cors
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+
+            services.AddControllersWithViews().AddNewtonsoftJson(options=>
+            options.SerializerSettings.ReferenceLoopHandling=Newtonsoft
+            .Json.ReferenceLoopHandling.Ignore
+            ).AddNewtonsoftJson(options=>options.SerializerSettings.ContractResolver= new DefaultContractResolver()
+            ); 
 
             services.AddControllersWithViews();
 
